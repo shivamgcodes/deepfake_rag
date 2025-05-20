c889510530147b0b82e330ce66d1be2b1bfe7e30
shivamgcodes <shivamguptaxia2@gmail.com>
2025-05-21 02:49:37 +0530

    config updates

--- config.py
+++ config.py
@@ -11,17 +11,18 @@ class parentConfig:
     learning_rate = 0.001
     num_workers = 4
     device ='cuda:0' if torch.cuda.is_available() else 'cpu'
-    plots_dir = "/kaggle/working/plots/"
-    logs_dir = "/kaggle/working/logs/"
-    model_dir = "/kaggle/working/model/"
+    plots_dir = "./working/plots/"
+    logs_dir = "./working/logs/"
+    model_dir = "./working/model/"
     transform =transforms.Compose([
     transforms.Resize(299),  # InceptionResNetV1 input size
     transforms.CenterCrop(299),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
-    def __init__(self):
-        # Create directories if they don't exist
-        os.makedirs(self.plots_dir, exist_ok=True)
-        os.makedirs(self.logs_dir, exist_ok=True)
-        os.makedirs(self.model_dir, exist_ok=True)
+
+
+# Create directories if they don't exist
+os.makedirs(parentConfig.plots_dir, exist_ok=True)
+os.makedirs(parentConfig.logs_dir, exist_ok=True)
+os.makedirs(parentConfig.model_dir, exist_ok=True)
